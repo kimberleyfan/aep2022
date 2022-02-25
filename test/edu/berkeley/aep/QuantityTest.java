cdea0f371f966405d596c1bd7a578d1923ff8237
Jez Humble <jez@jezhumble.net>
2022-02-25 14:11:02 -0800

    Refactored to extract superclass for ScaledQuantity

--- test/edu/berkeley/aep/QuantityTest.java
+++ test/edu/berkeley/aep/QuantityTest.java
@@ -7,74 +7,74 @@ import static org.junit.Assert.assertEquals;
 public class QuantityTest {
     @Test
     public void twelveInchesShouldEqualOneFoot() {
-        var twelveInches = new Quantity(12, Unit.INCH);
-        var oneFoot = new Quantity(1, Unit.FOOT);
+        var twelveInches = new ArithmeticQuantity(12, Unit.INCH);
+        var oneFoot = new ArithmeticQuantity(1, Unit.FOOT);
         assertEquals(twelveInches, oneFoot);
     }
 
     @Test
     public void threeFeetShouldEqualOneYard() {
-        var threeFeet = new Quantity(3, Unit.FOOT);
-        var oneYard = new Quantity(1, Unit.YARD);
+        var threeFeet = new ArithmeticQuantity(3, Unit.FOOT);
+        var oneYard = new ArithmeticQuantity(1, Unit.YARD);
         assertEquals(threeFeet, oneYard);
     }
 
     @Test
     public void oneMileShouldEqual1760yards() {
-        var one760Yard = new Quantity(1760, Unit.YARD);
-        var oneMile = new Quantity(1, Unit.MILE);
+        var one760Yard = new ArithmeticQuantity(1760, Unit.YARD);
+        var oneMile = new ArithmeticQuantity(1, Unit.MILE);
         assertEquals(one760Yard, oneMile);
     }
 
     @Test
     public void oneTablespoonShouldEqualThreeTeaspoons() {
-        var oneTbsp = new Quantity(1, Unit.TBSP);
-        var threeTsp = new Quantity(3, Unit.TSP);
+        var oneTbsp = new ArithmeticQuantity(1, Unit.TBSP);
+        var threeTsp = new ArithmeticQuantity(3, Unit.TSP);
         assertEquals(oneTbsp, threeTsp);
     }
 
     @Test
     public void oneOzShouldEqualTwoTbsp() {
-        var oneOz = new Quantity(1, Unit.OZ);
-        var threeTsp = new Quantity(2, Unit.TBSP);
+        var oneOz = new ArithmeticQuantity(1, Unit.OZ);
+        var threeTsp = new ArithmeticQuantity(2, Unit.TBSP);
         assertEquals(oneOz, threeTsp);
     }
 
     @Test
     public void twoInchesPlusTwoInchesShouldEqualFourInches() {
-        var twoInches = new Quantity(2, Unit.INCH);
-        var fourInches = new Quantity(4, Unit.INCH);
+        var twoInches = new ArithmeticQuantity(2, Unit.INCH);
+        var fourInches = new ArithmeticQuantity(4, Unit.INCH);
         assertEquals(fourInches, twoInches.add(twoInches));
     }
 
     @Test
     public void twoTablespoonsPlusOneOzShouldEqualTwelveTsp() {
-        var twoTbsp = new Quantity(2, Unit.TBSP);
-        var oneOz = new Quantity(1, Unit.OZ);
-        var twelveTsp = new Quantity(12, Unit.TSP);
+        var twoTbsp = new ArithmeticQuantity(2, Unit.TBSP);
+        var oneOz = new ArithmeticQuantity(1, Unit.OZ);
+        var twelveTsp = new ArithmeticQuantity(12, Unit.TSP);
         assertEquals(twelveTsp, twoTbsp.add(oneOz));
     }
 
     @Test
     public void oneHundredCelsiusShouldEqual212F() {
-        var oneHundredCelsius = new Quantity(100, Unit.CELSIUS);
-        var twoHundredTwelveFahrenheit = new Quantity(212, Unit.FAHRENHEIT);
+        var oneHundredCelsius = new ScaledQuantity(100, Unit.CELSIUS);
+        var twoHundredTwelveFahrenheit = new ScaledQuantity(212, Unit.FAHRENHEIT);
         assertEquals(oneHundredCelsius, twoHundredTwelveFahrenheit);
     }
 
     @Test
     public void zeroCelsiusShouldEqual32F() {
-        var zeroCelsius = new Quantity(0, Unit.CELSIUS);
-        var thirtyTwoFahrenheit = new Quantity(32, Unit.FAHRENHEIT);
+        var zeroCelsius = new ScaledQuantity(0, Unit.CELSIUS);
+        var thirtyTwoFahrenheit = new ScaledQuantity(32, Unit.FAHRENHEIT);
         assertEquals(zeroCelsius, thirtyTwoFahrenheit);
     }
 
     @Test(expected = RuntimeException.class)
     public void shouldNotBeAbleToAddIncomparableUnits() {
-        var twoTbsp = new Quantity(2, Unit.TBSP);
-        var oneInch = new Quantity(1, Unit.INCH);
+        var twoTbsp = new ArithmeticQuantity(2, Unit.TBSP);
+        var oneInch = new ArithmeticQuantity(1, Unit.INCH);
         try {
-            assertEquals(new Quantity(1, Unit.OZ), twoTbsp.add(oneInch));
+            assertEquals(new ArithmeticQuantity(1, Unit.OZ), twoTbsp.add(oneInch));
         } catch (Exception e) {
             throw new RuntimeException("I couldn't do anything about this!", e);
         }
